78385c30dc6da7ca3799e886da3e5b96de1db753
manish-infrrd <manish.c@inffrd.ai>
2018-12-05 00:16:02 +0530

    First commit new

---
+++ learn-git/src/main/java/com/manish/App.java
@@ -0,0 +1,19 @@
+package com.manish;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // First commit
+        
+        // Second commit
+        
+        // Third commit
+        
+        System.out.println( "Hello World!" );
+    }
+}
